f30594d1d5b2546deee943b03004f619bf49a3e3
Shreyansh <shreyansh.pettswood@gmail.com>
2017-05-16 00:22:18 +0530

    Changes for heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -157,3 +157,17 @@ SOCIALACCOUNT_PROVIDERS = {
     }
 }
 LOGIN_REDIRECT_URL = '/'
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
