99720da12438c63348e01b7d40cff95bfaadd6af
Shreyansh <shreyansh.pettswood@gmail.com>
2017-05-24 02:23:46 +0530

    Minor changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -167,6 +167,23 @@ ALLOWED_HOSTS = ['*']
 
 DEBUG = False
 
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,  'static'),# Add to this list all the locations containing your static files 
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+DEBUG = True
+
 try:
     from .local_settings import *
 except ImportError:
