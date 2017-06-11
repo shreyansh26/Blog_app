1a7a89fb2c8a1859a783c536048ad5b2d503c852
Shreyansh <shreyansh.pettswood@gmail.com>
2017-06-11 11:26:13 +0530

    Changed ordering

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
