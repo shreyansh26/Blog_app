b6f184d2ab2196ccf7271efd527a4d7b43cde077
Shreyansh <shreyansh.pettswood@gmail.com>
2017-06-11 11:31:43 +0530

    Order changed

--- blog/views.py
+++ blog/views.py
@@ -45,7 +45,7 @@ def post_edit(request, pk):
 
 @login_required
 def post_draft_list(request):
-    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 @login_required
